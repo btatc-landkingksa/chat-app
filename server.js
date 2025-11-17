edd522ddd2bfd4cfa78c7635a9d4475087edad98
Santosh-Paratsone-TechDev <santosh@parastoneglobal.com>
2025-11-17 09:49:02 +0530

    changes to server js

--- server.js
+++ server.js
@@ -10,40 +10,40 @@ const db = new sqlite3.Database('./chat.db', (err) => {
   if (err) throw err;
   console.log('Connected to SQLite');
 
-  db.run(
-    'CREATE TABLE messages (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, content TEXT)',
-    (err) => {
-      if (err) throw err;
+db.run(
+  'CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, content TEXT)',
+  (err) => {
+    if (err) throw err;
 
-      io.on('connection', (socket) => {
-        console.log('A user connected');
+    io.on('connection', (socket) => {
+      console.log('A user connected');
 
-        db.all('SELECT * FROM messages', (err, messages) => {
-          if (err) throw err;
-          socket.emit('load messages', messages);
-        });
-
-        socket.on('chat message', (msg) => {
-          db.run(
-            'INSERT INTO messages (username, content) VALUES (?, ?)',
-            [msg.username, msg.content],
-            (err) => {
-              if (err) throw err;
-              io.emit('chat message', msg);
-            }
-          );
-        });
-
-        socket.on('disconnect', () => {
-          console.log('A user disconnected');
-        });
+      db.all('SELECT * FROM messages', (err, messages) => {
+        if (err) throw err;
+        socket.emit('load messages', messages);
       });
 
-      app.use(express.static('public'));
-
-      server.listen(PORT, () => {
-        console.log(`Server listening on port ${PORT}`);
+      socket.on('chat message', (msg) => {
+        db.run(
+          'INSERT INTO messages (username, content) VALUES (?, ?)',
+          [msg.username, msg.content],
+          (err) => {
+            if (err) throw err;
+            io.emit('chat message', msg);
+          }
+        );
       });
-    }
-  );
+
+      socket.on('disconnect', () => {
+        console.log('A user disconnected');
+      });
+    });
+
+    app.use(express.static('public'));
+
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  }
+);
 });
